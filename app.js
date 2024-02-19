0d84fe302a4e7c302efc1c46dff5da1ea6ae6638
syedabdullahzaidi786 <160156087+syedabdullahzaidi786@users.noreply.github.com>
2024-02-19 22:36:52 +0500

    Exercise 12
    TypeScript Assignment.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 "use strict";
 let names = ['Syed Abdullah', 'Raheem', 'Rajab', 'Farjad'];
+const message = 'Welcome to Typescript code: ';
 for (let i = 0; i < names.length; i++) {
-    console.log(names[i]);
+    console.log(message + names[i]);
 }

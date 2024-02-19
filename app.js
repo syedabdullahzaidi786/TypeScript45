df411c04003a60c774cae295ad1cb9e2c22d5e56
syedabdullahzaidi786 <160156087+syedabdullahzaidi786@users.noreply.github.com>
2024-02-19 23:34:15 +0500

    Exercise 15
    TypeScript Assignment.

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 "use strict";
 let Guest_List = ['Rajab', 'Raheem', 'Farjad'];
 for (let i = 0; i < Guest_List.length; i++) {
-    console.log('Dear Mr. ' + Guest_List[i] + ',\n\nIt is own pleasure to invite you in our party.\n\nThank You!');
+    console.log('Dear Mr. ' + Guest_List[i] + ',\n\nIt is own pleasure to invite you in our party.\n\nThank You!\n\n');
 }
+let absent_Guest = 'Raheem';
+let new_Guest = 'Khurram';
+Guest_List[0] = new_Guest;
+for (let i = 0; i < Guest_List.length; i++) {
+    console.log('Dear Mr. ' + Guest_List[i] + ',\n\nIt is own pleasure to invite you in our party.\n\nThank You!\n\n');
+}
+console.log(`Mr. ${absent_Guest} is not coming to the party.`);

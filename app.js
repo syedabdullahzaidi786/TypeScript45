44a276b535cba74474c24ed5582fcb406fb0414f
syedabdullahzaidi786 <160156087+syedabdullahzaidi786@users.noreply.github.com>
2024-02-19 22:55:38 +0500

    Exercise 13
    TypeScript Assignment.

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 "use strict";
-let names = ['Syed Abdullah', 'Raheem', 'Rajab', 'Farjad'];
-const message = 'Welcome to Typescript code: ';
-for (let i = 0; i < names.length; i++) {
-    console.log(message + names[i]);
+let transportation = ['Motorcycle', 'Car', 'Train', 'Cycle'];
+for (let i = 0; i < transportation.length; i++) {
+    console.log('I would like to own a ' + transportation[i]);
 }
